2f8fc312ef1d8d2c7617e4fb338d781335599b33
Kristjan Fale <kristjan.fale@gmail.com>
2020-08-05 18:06:28 +0200

    Redux setup - Store and Provider

--- src/App.js
+++ src/App.js
@@ -3,17 +3,23 @@ import SearchBar from './components/layout/SearchBar';
 import Logs from './components/logs/Logs';
 import Buttons from './components/layout/Buttons';
 
+// REDUX
+import { Provider } from 'react-redux';
+import store from './store';
+
 import './App.css';
 
 function App() {
   return (
-    <Fragment>
-      <SearchBar />
-      <div className='container'>
-        <Logs />
-        <Buttons />
-      </div>
-    </Fragment>
+    <Provider store={store}>
+      <Fragment>
+        <SearchBar />
+        <div className='container'>
+          <Logs />
+          <Buttons />
+        </div>
+      </Fragment>
+    </Provider>
   );
 }
 
---
+++ src/reducers/index.js
@@ -0,0 +1,4 @@
+import { combineReducers } from 'redux';
+
+// Combine all reducers
+export default combineReducers({});
